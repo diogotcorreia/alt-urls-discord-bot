f52e7840dd91a2d4757a9b957ad4d101421cf2e2
Diogo Correia <me@diogotc.com>
2024-07-18 22:56:18 +0100

    chore: init repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Alternative URLs Discord Bot
+
+Adds a message command (right-click on message -> Apps -> Alt URLs) to your Discord server,
+which looks for social media links in the given message and replies with links to
+alternative frontends and embed services.
+Additionally, removes tracking parameters.
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          rust-analyzer
+          clippy
+        ];
+
+        shellHook = ''
+          export RUST_LOG=debug
+        '';
+      };
+    });
+}
